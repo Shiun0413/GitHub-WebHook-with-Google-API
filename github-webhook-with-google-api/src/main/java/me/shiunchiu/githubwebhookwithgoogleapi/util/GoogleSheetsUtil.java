a7065bb386352a99e194399305cf9f5d1ea1522e
shiun0413 <a0923183408@gmail.com>
2023-06-07 00:37:52 +0800

    feat(Util): add GoogleSheetsUtil

---
+++ github-webhook-with-google-api/src/main/java/me/shiunchiu/githubwebhookwithgoogleapi/util/GoogleSheetsUtil.java
@@ -0,0 +1,42 @@
+package me.shiunchiu.githubwebhookwithgoogleapi.util;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+public class GoogleSheetsUtil {
+  private static final String APPLICATION_NAME = "Google Sheets Example";
+  private static HttpTransport httpTransport;
+
+  private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
+
+  static {
+    try {
+      httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    } catch (GeneralSecurityException | IOException e) {
+      // Handle exception
+    }
+  }
+
+  public static Sheets getSheetsService() throws IOException {
+    JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+    GoogleCredentials credential = GoogleCredentials.fromStream(new FileInputStream("path/to/your/serviceAccountKey"))
+        .createScoped(SCOPES);
+    HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credential);
+
+    return new Sheets.Builder(httpTransport, jsonFactory, requestInitializer)
+        .setApplicationName(APPLICATION_NAME)
+        .build();
+  }
+}
